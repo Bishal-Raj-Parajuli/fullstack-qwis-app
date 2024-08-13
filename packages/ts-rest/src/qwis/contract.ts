7bc6984cb8a76b90d61304b8a88a7e3257a87787
Bishal-Raj-Parajuli <hello76tortugaa@protonmail.com>
2024-08-13 16:08:59 +1000

    feat: added ts-rest contract and zod types

---
+++ packages/ts-rest/src/qwis/contract.ts
@@ -0,0 +1,10 @@
+import { initContract } from "@ts-rest/core";
+import { userRoutes } from "./routes/user.routes";
+import { qwisRoutes } from "./routes/qwis.routes";
+
+export const API = initContract().router(
+    {
+        User: userRoutes.routes,
+        Qwis: qwisRoutes.routes
+    }
+)
---
+++ packages/ts-rest/src/qwis/routes/user.routes.ts
@@ -0,0 +1,43 @@
+import { initContract } from "@ts-rest/core";
+import z from "zod";
+import { zUser } from "../types";
+
+const c = initContract();
+
+const zgetUserListRes = z.object({
+    status: z.literal('OK'),
+    data: z.array(zUser)
+})
+
+const zcreateUserReq = z.object({
+    userName: z.string(),
+    totalPoint: z.number().default(0),
+    country: z.string()
+})
+
+const zcreateUserRes = z.object({
+    status: z.literal('OK'),
+    data: z.array(zUser)
+})
+
+export const userRoutes = {
+    routes: c.router({
+        getUserList: {
+            method: 'GET',
+            path: '/users',
+            responses: {
+                200: zgetUserListRes
+            },
+            summary: "Get list of users"
+        },
+        createUser: {
+            method: 'POST',
+            path: '/user',
+            body: zcreateUserReq,
+            responses: {
+                201: zcreateUserRes
+            },
+            summary: "Create user"
+        }
+    })
+}
