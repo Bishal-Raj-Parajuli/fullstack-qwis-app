56b291f96b6863a4d33c1a6061c2731cfd3af0b4
Bishal-Raj-Parajuli <hello76tortugaa@protonmail.com>
2024-08-18 15:54:43 +1000

    update 2

--- apps/api/main.ts
+++ apps/api/main.ts
@@ -1,5 +1,5 @@
 import { NestFactory } from '@nestjs/core';
-import { AppModule } from './app.module';
+import { AppModule } from './src/app.module';
 import { generateOpenApi } from '@ts-rest/open-api';
 import { API } from '@qwis/ts-rest';
 import { SwaggerModule } from '@nestjs/swagger';
